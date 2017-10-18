dc0436fb6d3b617f9324384fbbc846c7c8ce3cd2
hengwu <youchuangqiba@163.com>
2017-10-18 21:06:01 +0800

    upd8: domready js => ts.

--- src/domready.ts
+++ src/domready.ts
@@ -1,17 +1,17 @@
-const fns = [];
+const fns: Array<Function> = [];
 
 let ready = (window.document.documentElement.doScroll ? /^loaded|^c/ : /^loaded|^i|^c/).test(window.document.readyState);
 
-let listener;
+let listener: Function;
 
 !ready &&
 window.document.addEventListener &&
 window.document.addEventListener('DOMContentLoaded', listener = function () {
-    window.document.removeEventListener('DOMContentLoaded', listener);
+    window.document.removeEventListener('DOMContentLoaded', <EventListener>listener);
     ready = true;
     while (listener = fns.shift()) listener();
 });
 
-export default function domReady(fn) {
+export default function domReady(fn: Function) {
     ready ? window.setTimeout(fn, 0) : fns.push(fn);
 }
